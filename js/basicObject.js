92615a78db50638403660102e29177495d55a87e
Ann-Kathrin Tissen <58940176+AnnKathrinTissen@users.noreply.github.com>
2020-06-08 09:26:26 +0200

    added basicObject

---
+++ js/basicObject.js
@@ -0,0 +1,44 @@
+export default class BasicObject {
+  constructor(x, y, width, height, radius, rectColor, title, sizeText, image) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.radius = radius;
+    this.rectColor = rectColor;
+    this.title = title;
+    this.sizeText = sizeText;
+    this.image = image;
+  }
+
+  display() {
+    fill(this.rectColor);
+    rect(this.x, this.y, this.width, this.height, this.radius);
+    image(this.image, this.x, this.y);
+    textSize(this.sizeText);
+    textAlign(CENTER, CENTER);
+    text(this.title, this.x + this.width / 2, this.y + this.height / 2);
+  }
+
+  hitTest(x, y) {
+    if (
+      x > this.x &&
+      x < this.x + this.width &&
+      y > this.y &&
+      y < this.y + this.height
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  clicked() {
+    console.log("Test");
+  }
+
+  mouseClicked() {
+    if (this.hitTest(mouseX, mouseY)) {
+      this.clicked();
+    }
+  }
+}
