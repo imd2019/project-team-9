30818461fafc8794f70d24d18303b5e31ec9e85a
Ann-Kathrin Tissen <58940176+AnnKathrinTissen@users.noreply.github.com>
2020-07-06 19:03:49 +0200

    update draggable

--- js/draggable.js
+++ js/draggable.js
@@ -7,7 +7,6 @@ export default class Draggable extends BasicObjectImage {
     this.productivity = 0;
     this.workingHours = 0;
     this.isAvailable = true;
-    this.satisfaction = 0;
     this.totalActiveTime = 0;
     this.isDragged = false;
     this.alreadyChecked = false;
@@ -37,19 +36,6 @@ export default class Draggable extends BasicObjectImage {
     if (this.hitTest(mouseX, mouseY)) {
       this.clickTest = true;
     }
-  }
-
-  setPreferences() {
-    // hier muss noch rein, welche mobility option die Zufriedenheit steigert oder sinken lässt, dazu muss ich aber erstmal schauen, wie die mobility options aufgebaut sind
-    this.satisfaction = Math.floor(random(41, 70));
-  }
-
-  checkSatisfaction() {
-    if (this.workingHours > 10) {
-      this.satisfaction -= 20;
-    }
-
-    // hier kommt noch: wenn die mobilityOption den preferences entspricht, dann steigt die Zufriedenheit
   }
 
   checkWorkingHours(activeTime, inactiveTime) {
